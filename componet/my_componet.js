c950f50be00c258aa775bfbdcebc7d80b38b1eea
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-03-20 11:54:48 -0500

    feat: :sparkles: v-model
    practicas basicas con directivas, directiva v-model

--- componet/my_componet.js
+++ componet/my_componet.js
@@ -1,9 +1,8 @@
 export default{
     data(){
         return{
-            basico : `<p style="color:orange;">Apolo</p>`,
-            intermedio: `<p style="color:green;">Artemis</p>`,
-            avanzado: `<p style="color:red;">Sputnik</p>`
+            mensaje: "lorem"
+           
         }
     }
 }
