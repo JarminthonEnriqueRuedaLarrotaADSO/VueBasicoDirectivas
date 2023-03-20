d19d08da4aca5024e8058d25af0c480301c05566
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-03-20 17:24:47 -0500

    feat: :sparkles: v-for
    practicas directivas vue, directivas ciclos v-for

--- componet/my_componet.js
+++ componet/my_componet.js
@@ -1,11 +1,11 @@
 export default{
     data(){
         return{
-           mensaje :  `<em>Apolo</em>` ,
-           mensaje2 : `<mark>Artemis</mark>`,
-           mensaje3 : `<u>Sputnik</u>`,
-           mensaje4 : `NO HA INTRODUCIDO UN VALOR`,
-           opcion :null
+          lista:[
+            "Apolo",
+            "Artemis",
+            "Sputnik"
+          ]
         }
     }
 }
