0cf5f0f7c7e95ede81a02cfddee25582580295a5
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-03-21 12:01:53 -0500

    feat: :sparkles: Objetos
    Enlazar completamente una etiqueta HTML con un objeto

--- componet/my_componet.js
+++ componet/my_componet.js
@@ -1,10 +1,10 @@
 export default{
     data(){
         return{
-          atributo: "src",
-          imagen:"https://i.blogs.es/0ec27f/memess/1366_2000.jpg",
-          atributo2: "alt",
-          text:"meme"
+          imagen:{
+            src:"https://i.blogs.es/0ec27f/memess/1366_2000.jpg",
+            title:"meme"
+          }
         }
     }
 }
