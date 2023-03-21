8d00dee10bc0241a9dae9493e242508f912bafab
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-03-21 11:59:00 -0500

    feat: :sparkles: Dinamico

--- componet/my_componet.js
+++ componet/my_componet.js
@@ -1,7 +1,9 @@
 export default{
     data(){
         return{
+          atributo: "src",
           imagen:"https://i.blogs.es/0ec27f/memess/1366_2000.jpg",
+          atributo2: "alt",
           text:"meme"
         }
     }
