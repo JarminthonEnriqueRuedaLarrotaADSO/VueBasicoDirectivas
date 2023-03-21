057029969a47454d413e6db67fabb361c2440511
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2023-03-21 15:52:18 -0500

    feat: :sparkles: Class
    casos especiales con el atributo class de una etiqueta, utilizada para añadir clases CSS a un elemento HTML.

--- componet/my_componet.js
+++ componet/my_componet.js
@@ -1,10 +1,13 @@
 export default{
     data(){
         return{
-          imagen:{
-            src:"https://i.blogs.es/0ec27f/memess/1366_2000.jpg",
-            title:"meme"
-          }
+          clasName: "box",
+          clasName2: ['box','box2'],
+          clasName3: {
+            box:true,
+            box2:false,
+            box3:true
+           }
         }
     }
 }
